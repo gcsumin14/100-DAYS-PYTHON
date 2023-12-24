7e47fb5eb71efd9f84152f25ff3d2e5b5b351f01
gcsumin14 <149852844+gcsumin14@users.noreply.github.com>
2023-12-24 11:38:50 +0545

    refactor: :art: small change

--- DAY1.py
+++ DAY1.py
@@ -3,7 +3,7 @@
 #1. CREATE  a greeting for your program
 print("WELCOME TO BAND NAME GENERATOR")
 #2.ASK THE USER FOR THE CITY THAT THEY LIVE IN
-city=input("WHATS THE CITY YOU GREW UP IN???\n")
+city=input("WHATS THE CITtY YOU GREW UP IN???\n")
 #3. Ask user for the name of the pet
 pet=input("Whats your pet name??\n")
 print("YOUR BAND NAME COULD BE"+city+ " " +pet)
